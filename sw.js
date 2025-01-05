11a4f98f99c4419a735d6364ad12e847687b33ec
JhonAlexanderCalderon <49382446+JhonAlexanderCalderon@users.noreply.github.com>
2025-01-05 16:44:15 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'issue-reporter-cache-v1';
+const urlsToCache = [
+  '/',
+  '/styles.css',
+  '/script.js',
+  '/concept-labs-logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
